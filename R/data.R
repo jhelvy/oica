7a7ebf92fdb805a3fc29c4ce1f665548d77930f8
John Helveston <john.helveston@gmail.com>
2022-04-22 12:38:10 -0400

    init raw data and old code

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' Description
+#'
+#' Detailed description
+#'
+#' @format
+#' Variable | Description
+#' -------- | ---------------------------------------------
+#' `var` | Description
+#'
+#' @docType data
+#'
+#' @usage data(dfName)
+#'
+#' @keywords datasets
+#'
+#' @source Raw data downloaded from \href{url}{text}
+#'
+#' @examples
+#' data(dfName)
+#'
+#' head(dfName)
+"dfName"
